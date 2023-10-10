44afaad2e0e7ddf78bef4c8f78a08be217c3275d
Arthur SZANTYR <szantyrarthur.pro@gmail.com>
2023-10-10 09:28:58 +0200

    client class

--- client/client.py
+++ client/client.py
@@ -1,5 +1,6 @@
 # Import socket module
 import socket
+import pygame
 
 ###################
 
@@ -11,56 +12,43 @@ port = 12345
 
 class Player():
     
-	def __init__(self, x, y, direction, color=(255,0,0)) -> None:
+	def __init__(self,id, x, y, direction, color=(255,0,0)) -> None:
+
+		self.id = id
+
 		self.x = x
 		self.y = y
 		self.direction = direction #haut, bas, gauche, droite
-		self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-		self.s.connect((host_ip, port))
+		self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		self.socket.connect((host_ip, port))
 
-	def input(self): #return new direction
-		new_dir = 
+	def send_to_server(self, data):
+		self.socket.send(str.encode(data))
 
-"""
-def Main():
-	# local host IP '127.0.0.1'
-    # stan ip : 172.21.72.136
-	host = '172.21.72.136'
+	def move(self): #return new direction
+		keys = pygame.key.get_pressed()
 
-	# Define the port on which you want to connect
-	port = 8080
+		if keys[pygame.K_RIGHT]:
+			self.direction = "R"
+			self.x += 1
 
-	s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		if keys[pygame.K_LEFT]:
+			self.direction = "L"
+			self.x -=1
+		
+		if keys[pygame.K_UP]:
+			self.direction = "U"
+			self.y += 1
 
-	# connect to server on local computer
-	s.connect((host,port))
+		if keys[pygame.K_DOWN]:
+			self.direction = "D"
+			self.y -=1
 
-	# message you send to server
-	message = "shaurya says geeksforgeeks"
-	while True:
+	def data_network_format(self):
+		return f"{self.x},{self.y},{self.direction},{self.id}"
 
-		# message sent to server
-		s.send(message.encode('ascii'))
+		
 
-		# message received from server
-		data = s.recv(1024)
 
-		# print the received message
-		# here it would be a reverse of sent message
-		print('Received from the server :',str(data.decode('ascii')))
 
-		# ask the client whether he wants to continue
-		ans = input('\nDo you want to continue(y/n) :')
-		if ans == 'y':
-			continue
-		else:
-			break
-	# close the connection
-	s.close()
-
-	#test123
-
-if __name__ == '__main__':
-	Main()
-"""
