a8932556aa2d9215df4284caf6f933ae92acf6f8
Arthur SZANTYR <szantyrarthur.pro@gmail.com>
2023-10-10 10:14:48 +0200

    modified client.py

--- client/client.py
+++ client/client.py
@@ -6,7 +6,7 @@ import pygame
 
 #'stan ip' : 172.21.72.136
 host_ip = '172.21.72.136'
-port = 12345
+port = 7777
 
 ###################
 
@@ -21,7 +21,9 @@ class Player():
 		self.direction = direction #haut, bas, gauche, droite
 
 		self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		
 		self.socket.connect((host_ip, port))
+		self.socket.recv(2048).decode() #reste ne écoute sur le serveur sinon deco direct
 
 	def send_to_server(self, data):
 		self.socket.send(str.encode(data))
@@ -47,6 +49,9 @@ class Player():
 
 	def data_network_format(self):
 		return f"{self.x},{self.y},{self.direction},{self.id}"
+	
+p1 = Player(1,0,2,"R")
+p1.send_to_server(p1.data_network_format())
 
 		
 
