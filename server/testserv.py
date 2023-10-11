e896e1bbc51d6612890a7be07f0d7a54e83e1932
Stanislas-Gadeceau <stanislasgadeceau@hotmail.com>
2023-10-11 15:17:43 +0200

    V1 jeu Server

--- server/testserv.py
+++ server/testserv.py
@@ -6,7 +6,8 @@ import numpy as np
 import json
 
 # Paramètres du jeu
-FPS = 30 # Fréquence de vérification (25 fois par seconde)
+
+FPS = 3 # Fréquence de vérification (25 fois par seconde)
 
 # Temps entre chaque vérification
 delta_time = 1 / FPS
@@ -28,18 +29,18 @@ class Game:
     def __init__(self):
         self.width = 0
         self.height = 0
-        self.players = []
+        self.players : list[Player] = []
         self.plateau = []
 
     def send_game_state_to_clients(self):
         all_players_data = []
-    
+
         for player in self.players:
             player_data = f"{player.id},{player.x},{player.y},{player.d}"
             all_players_data.append(player_data)
-    
+
         players_data = ";".join(all_players_data)  # Joindre les données de tous les joueurs avec des points-virgules
-    
+
         for client in connected_clients:
             try:
                 client.send((players_data + "!").encode("utf-8"))
@@ -66,7 +67,7 @@ class Game:
         # Obtenez le nom du socket pour utiliser comme ID du joueur
             #player_id = c.getpeername()
             client_ip, client_port = c.getpeername()
-            self.players.append(Player(x=x, y=0, d="h", id=client_port))
+            self.players.append(Player(x=x, y=0, d="U", id=client_port))
             print(c.getpeername())
             print(client_port)
 
@@ -75,28 +76,31 @@ class Game:
     def deplacement(self, player):
         x, y, d, id = player.x, player.y, player.d, player.id
         new_x, new_y = x, y
-
-        if d == "h":
+    
+        if d == "U":
             new_y = y - 1
-        elif d == "b":
+        elif d == "D":
             new_y = y + 1
-        elif d == "g":
+        elif d == "L":
             new_x = x - 1
-        elif d == "d":
+        elif d == "R":
             new_x = x + 1
-
+    
         if new_x < 0 or new_x >= self.width or new_y < 0 or new_y >= self.height:
             return True  # Collision avec les bords du plateau
-
-        # Vérifie si le joueur entre en collision avec d'autres joueurs ou des obstacles
+    
+        # Vérifiez si le joueur entre en collision avec d'autres joueurs ou des obstacles
         if self.plateau[new_y][new_x] != 0:
-            return True 
-        
-        self.plateau[y][x] = id
-        self.plateau[new_y][new_x] = id
-        player.x, player.y = new_x, new_y
-
+            return True  # Collision avec un autre joueur ou un obstacle
+    
+        # Mettez à jour les positions sur le plateau
+        self.plateau[y][x] = 0  # Ancienne position
+        self.plateau[new_y][new_x] = id  # Nouvelle position
+    
+        player.x, player.y = new_x, new_y  # Mettez à jour les coordonnées du joueur
+    
         return False
+
 
 
 
@@ -104,15 +108,17 @@ class Game:
         while True:
             # Heure de départ de la boucle
             start_time = time.time()
-
+           # print("dans le jeu")
             # Votre logique de jeu ici
             for player in self.players:
-                if not self.deplacement(player):
-                    # Le joueur n'a pas rencontré de collision, continuez le mouvement
-                    pass
-                else:
-                    # Collision, le joueur est éliminé ou doit être géré
-                    pass
+                self.deplacement(player)
+               # print("eplacement ?")
+               # if not self.deplacement(player):
+               #     # Le joueur n'a pas rencontré de collision, continuez le mouvement
+               #     pass
+               # else:
+               #     # Collision, le joueur est éliminé ou doit être géré
+               #     pass
 
             # Appelez send_game_state_to_clients pour envoyer les nouvelles positions des joueurs
             self.send_game_state_to_clients()
@@ -128,7 +134,7 @@ class Game:
 
 socket_to_player_id = {}
 
-def threaded(c, game):
+def threaded(c, game : Game):
     global connected_clients, socket_to_player_id
 
     try:
@@ -146,12 +152,16 @@ def threaded(c, game):
             parts = data.split(",")
             if len(parts) == 2:
                 socket_id, new_direction = parts  # Séparez les deux parties
-
+                print(socket_id+new_direction)
+                
                 # Recherchez le joueur correspondant à l'ID du socket
                 for player in game.players:
-                    if player.id == int(socket_id):
+                    print(f"Socket id {socket_id} ")
+                    print(f"Client id {player.id} ")
+                    if int(player.id) == int(socket_id):
+                        print("Im in")
                         player.update_direction(new_direction)
-                        break
+                        # break
 
     except ConnectionResetError:
         print(f"Client {client_name} disconnected")
@@ -206,8 +216,16 @@ def Main():
     game_thread = threading.Thread(target=game.Jeu)
     game_thread.daemon = True
     game_thread.start()
+    #print("caca")
+    #game.Jeu()
+    #jeuencours = True
+    #while jeuencours:
+    #    for player22 in game.players :
+    #        new_x, new_y = receive_player_movement_info(player22)  # Remplacez cette ligne par la façon dont vous recevez les coordonnées
+    #        player22.deplacement(new_x, new_y)
+       
 
-    # Laissez le jeu s'exécuter indéfiniment
+
     game_thread.join()
 
 if __name__ == '__main__':
