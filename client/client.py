ef0eb956b926b9e09c97e41f8ff48ef8c905174f
Arthur SZANTYR <szantyrarthur.pro@gmail.com>
2023-10-11 12:06:37 +0200

    ajout commentaire

--- client/client.py
+++ client/client.py
@@ -15,21 +15,40 @@ port = 7778
 ###################
 class Client:
     def __init__(self) -> None:
+        
+        #creation d'un socket par client a chaque instanciation 
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #connexion au serveur du socket 
         self.client_socket.connect((host_ip, port))
 
+        #recuperation de l'ip du client et du port client
         self.client_ip, self.client_port = self.client_socket.getsockname()
 		
-#def connect_to_server():
-#    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#    client_socket.connect((host_ip, port))
-#    return True
 
-def send_to_server(client_socket: "socket", data: str):
+def send_to_server(client_socket: "socket", data: str) -> bool:
+    """
+    Fonction permettant de d'envoyer un paquet au serveur - avec le socket crée 
+
+    Args:
+        client_socket: "socket"  ->  socket du client pour communiquer avec le serveur
+        data: str -> data a envoyée 
+
+    Returns:
+        bool ->  return true quand le message a correctement été envoyé
+    """
     client_socket.send(str.encode(data))
     return True
 
-def get_new_direction():
+def get_new_direction() -> str:
+    """
+    Fonction permettant de recuperer la nouvelle direction souhaité par le joueur - en fonction d'evenement sur le clavier ou non
+
+    Args:
+
+    Returns:
+        str ->  retourne la direction souhaité - si aucun evenement clavier return None
+
+    """
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
@@ -53,167 +72,108 @@ class Fenetre:
         self.matrice = np.zeros((30, 30), dtype=int)  # Créez une matrice de dimensions n x n remplie de zéros de type entier
 
 
-    def render_matrix(self, player_data, color_dict):
+    def render_matrix(self, player_data: dict, color_dict: dict) -> None:
+        """
+    Fonction permettant de mettre a jour la matrice courante du jeu et la fenetre pygame
+
+    Args:
+        player_data: dict ->  dictionnaire contenant les nouvelles positions de tous les joueurs envoyés par le serveur
+        color_dict: dict -> dictionnaire généré en debut de partie qui atribue une couleur a chaque joueur
+
+    Returns:
+
+    """
         # Remplir l'écran avec une couleur blanche
         self.fenetre.fill((255, 255, 255))
 
-        # Mettre id des joueurs sur la matrice
+        # Update de la matrice courante du jeu
         for player_id, (x, y, d) in player_data.items():
-            print(player_id)
-            print(y+" , "+x)
             self.matrice[int(y)][int(x)] = player_id
-            #self.matrice[3][9] = 1
-            #self.matrice[3][6] = 2
-            print(f"{player_id}  ->   {self.matrice[int(y)][int(x)]}")
             
-
         # Boucle pour parcourir la matrice et dessiner les cellules
         for y in range(len(self.matrice)):
             for x in range(len(self.matrice[y])):
                 position = self.matrice[y][x]
                 if position != 0:
-                    print(f"position : {position}")
-                    #pygame.draw.rect(self.fenetre, color_dict[position], (y*10 , x*10 , 10, 10))  #probleme sur le *10 ammene des hors cadres
-                    pygame.draw.rect(self.fenetre, color_dict[position], (y*10 , x*10 , 10, 10))  #probleme sur le *10 ammene des hors cadres
-                    print(f" after draw : {y} et {x}")
+                    pygame.draw.rect(self.fenetre, color_dict[position], (y*5 , x*5 , 5, 5))  #probleme sur le facteur *10 ammene des hors cadres
         pygame.display.flip()
 
-#f = Fenetre()
-#while True:
-#    # Supposer que vous recevez les nouvelles positions des joueurs du serveur sous forme de dictionnaire
-#    player_data = {1: (random.randrange(0,40), random.randrange(0,40)), 2: (random.randrange(0,40), random.randrange(0+1,40-1))}  # Exemple de nouvelles données de joueurs
-#    print(player_data)
-#    f.render_matrix()
-#    time.sleep(1)
+def decrypt_data(data_brut: str)-> dict:
+    """
+    Fonction permettant de convertir le paquet recu par le serveur en dictionnaire
 
-def decrypt_data(data):
+    Args:
+        data: str -> data recu par le serveur sous forme de string
 
-    data = data.split('!')
-    data = data[0]
+    Returns:
+        dict ->  return un dictionnaire avec key: client_port et value: la nouvelle position
+    """
 
-    lines = data.split(';')
+    data_brut = data_brut.split('!')
+    data_brut = data_brut[0]
+
+    data_players = data_brut.split(';')
     positions = {}
 
-    for line in lines:
-        elements = line.split(',')
-        key = int(elements[0])
-        values = tuple(elements[1:])
-        positions[key] = values
+    for data_player in data_players:
+        elements = data_player.split(',')
+        key = int(elements[0])   #selectionne le port client identifié par le serveur
+        position = tuple(elements[1:])  #selectionne la position pour la mettre en value
+        positions[key] = position
 
     return positions
 
-def generate_colors(player_data):
+def generate_colors(player_data: dict)-> dict:
+    """
+    Fonction permettant de generer un dictionaire qui attribue une couleur aléatoire a chaque player
+
+    Args:
+        player_data: dict -> data recu par le serveur qui identifie les joueurs par leur port client
+
+    Returns:
+        dict ->  return un dictionnaire avec key: client_port et value: couleur
+    """
+    
     color_dict = {}
     for player_id, (x, y, d) in player_data.items():
         color_dict[player_id] = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
     return color_dict 
 
 
-def run():
+def run()-> None:
+    """
+    Fonction qui gere les différentes etapes de jeu
+
+    Args:
+
+    Returns:
+    
+    """
+
     c = Client()
     f = Fenetre()
-    #for i in range(10):
-    #    send_to_server(c.client_socket, "hello stanos")
-    #print("rayyy")
-    i=0
+
+    i=0 #pour identifier le premier tour de jeu
+
+    #boucle de jeu
     while True:
-        #send_to_server(c.client_socket, "hello stanos")
-        data = c.client_socket.recv(1024)  # 1024 est la taille du tampon, ajustez-la en fonction de vos besoins
+        data = c.client_socket.recv(1024)  # 1024 est la taille max du message
         if not data:
             break
-        #print("Message reçu du serveur :", data.decode()) 
 
         player_data = decrypt_data(data.decode())
 
-        if i == 0:
+        if i == 0:  #au premier tour de jeu - generation du dictionnaire de couleur
           color_dict = generate_colors(player_data)
           i+=1
 
         print(player_data)
         f.render_matrix(player_data, color_dict)
 
-        new_d = get_new_direction()
-        if new_d != None:
+        new_d = get_new_direction()   #verifie les evenements clavier si pas de retour pas d'envoi au serveur
+        if new_d != None:   
             send_to_server(c.client_socket, f"{c.client_port},{new_d}")
-
-
-        
-    #wait "start" from server
-
-    #while True:
-    #    new_direction = get_new_direction()
-    #    if new_direction != None:
-    #        send_to_server(new_direction)
-
 
 run()
 
     
-
-
-			
-"""
-class Player():
-    
-	def __init__(self,id, x, y, direction, color=(255,0,0)) -> None:
-
-		#self.id = id
-
-		self.x = x
-		self.y = y
-		self.direction = direction #haut, bas, gauche, droite
-
-		self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-		
-		self.socket.connect((host_ip, port))
-		print("c'est moiu")
-		#self.socket.recv(2048).decode() #reste ne écoute sur le serveur sinon deco direct
-		print("je suis ici")
-
-	def send_to_server(self, data):
-		print("la")
-		self.socket.send(str.encode(data))
-		print("amdoulad")
-
-	def move(self): #return new direction
-		keys = pygame.key.get_pressed()
-
-		if keys[pygame.K_RIGHT]:
-			self.direction = "R"
-			self.x += 1
-
-		if keys[pygame.K_LEFT]:
-			self.direction = "L"
-			self.x -=1
-		
-		if keys[pygame.K_UP]:
-			self.direction = "U"
-			self.y += 1
-
-		if keys[pygame.K_DOWN]:
-			self.direction = "D"
-			self.y -=1
-
-	def data_network_format(self):
-		return f"{self.x},{self.y},{self.direction},{self.socket.getsockname()[1]}"
-	
-p1 = Player(1,0,2,"R")
-
-
-while True:
-	p1.send_to_server(p1.data_network_format())
-
-p1.send_to_server(p1.data_network_format())
-p1.send_to_server(p1.data_network_format())
-
-#while True:
-#	p1.send_to_server("mama")
-#p1.send_to_server(p1.data_network_format())
-#p1.send_to_server(p1.data_network_format())
-#p1.send_to_server(p1.data_network_format())
-
-
-"""		
-
-
-
